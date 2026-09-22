c5de318f234ca8437a34e25b8c358fdf6a4ad9c3
zhanjunxiang <907140412@qq.com>
2026-09-23 06:17:04 +0800

    长期积累学习项目

---
+++ java/src/main/java/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        //打印你好
+        System.out.println("你好");
+    }
+
+
+    // 测试方法
+    public void test() {
+        System.out.println("你好");
+    }
+
+
+
+}
